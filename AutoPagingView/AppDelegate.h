bac2bae6864177d5d59b349dcfc1ab5bce8a1d22
Yundi Huang <kenkaka.hyd@gmail.com>
2015-02-04 21:37:07 -0500

    Initial Commit

---
+++ AutoPagingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoPagingView
+//
+//  Created by Ken Huang on 2015-02-04.
+//  Copyright (c) 2015 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoPagingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoPagingView
+//
+//  Created by Ken Huang on 2015-02-04.
+//  Copyright (c) 2015 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
